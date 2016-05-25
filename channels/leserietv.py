a63b22ac6b6191324594138941af238725550967
costaplus <costaplus@gmail.com>
2016-05-25 13:08:53 +0200

    Add News

--- channels/leserietv.py
+++ channels/leserietv.py
@@ -39,11 +39,23 @@ def mainlist(item):
     logger.info("[leserietv.py] mainlist")
     itemlist =[]
     itemlist.append(Item(channel=__channel__,
+                         action="novita",
+                         title="[COLOR yellow]Novità[/COLOR]",
+                         url="http://www.leserie.tv/streaming/",
+                         thumbnail="http://www.ilmioprofessionista.it/wp-content/uploads/2015/04/TVSeries3.png",
+                         fanart="http://www.macroidee.it/wp-content/uploads/2015/06/migliori-serie-da-vedere.jpg"))
+    itemlist.append(Item(channel=__channel__,
                          action="lista_serie",
                          title="[COLOR azure]Tutte le serie[/COLOR]",
                          url="http://www.leserie.tv/streaming/",
                          thumbnail="http://www.ilmioprofessionista.it/wp-content/uploads/2015/04/TVSeries3.png",
                          fanart="http://www.macroidee.it/wp-content/uploads/2015/06/migliori-serie-da-vedere.jpg"))
+
+    itemlist.append(Item(channel=__channel__,
+                         title="[COLOR azure]Categorie[/COLOR]",
+                         action="categorias",
+                         url=host,
+                         thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"))
 
     itemlist.append(Item(channel=__channel__,
                          action="top50",
@@ -58,9 +70,54 @@ def mainlist(item):
                          url="http://www.leserie.tv/index.php?do=search",
                          thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search",
                          fanart="http://www.kaushik.net/avinash/wp-content/uploads/2010/02/search_engine.png"))
+
     return itemlist
 #=================================================================
 
+
+#-----------------------------------------------------------------
+def novita(item):
+    logger.info("streamondemand.laserietv novità")
+    itemlist = []
+
+    data = scrapertools.cache_page(item.url)
+
+
+    patron ='<div class="carousel-item-cover">[^=]+="(.*?)">.*?src="(.*?)"[^=]+="(.*?)">'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    for scrapedurl,scrapedthumbnail,scrapedtitle in matches:
+        if (DEBUG): logger.info("title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+
+
+        try:
+            tmdbtitle = scrapedtitle
+            plot, fanart, poster, extrameta = info(tmdbtitle)
+
+            itemlist.append(Item(channel=__channel__,
+                                 thumbnail=poster,
+                                 fanart=fanart if fanart != "" else poster,
+                                 extrameta=extrameta,
+                                 plot=str(plot),
+                                 action="episodi",
+                                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                                 url=scrapedurl,
+                                 fulltitle=scrapedtitle,
+                                 show=scrapedtitle,
+                                 folder=True))
+        except:
+
+             itemlist.append(Item(channel=__channel__,
+                                  action="episodi",
+                                  title=scrapedtitle,
+                                  url=scrapedurl,
+                                  thumbnail="",
+                                  fulltitle=scrapedtitle,
+                                  show=scrapedtitle))
+
+    return itemlist
+#=================================================================
 
 #-----------------------------------------------------------------
 def lista_serie(item):
@@ -118,6 +175,39 @@ def lista_serie(item):
         itemlist.append(Item(channel=__channel__, action="lista_serie", title="[COLOR orange]Successivo>>[/COLOR]", url=paginaurl,thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",folder=True))
         itemlist.append(Item(channel=__channel__, action="HomePage", title="[COLOR yellow]Torna Home[/COLOR]", folder=True))
     return itemlist
+#=================================================================
+
+#-----------------------------------------------------------------
+def categorias(item):
+    logger.info("streamondemand.laserietv categorias")
+    itemlist = []
+
+    data = scrapertools.cache_page(item.url)
+
+    # Narrow search by selecting only the combo
+    bloque = scrapertools.get_match(data, '<ul class="dropdown-menu cat-menu">(.*?)</ul>')
+
+    # The categories are the options for the combo
+    patron = '<li ><a href="([^"]+)">(.*?)</a></li>'
+    matches = re.compile(patron, re.DOTALL).findall(bloque)
+
+    for scrapedurl, scrapedtitle in matches:
+        scrapedurl = urlparse.urljoin(item.url, scrapedurl)
+        scrapedthumbnail = ""
+        scrapedplot = ""
+        if (DEBUG): logger.info(
+                "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="lista_serie",
+                     title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                     url=scrapedurl,
+                     thumbnail=scrapedthumbnail,
+                     plot=scrapedplot))
+
+    return itemlist
+
+
 #=================================================================
 
 #-----------------------------------------------------------------
