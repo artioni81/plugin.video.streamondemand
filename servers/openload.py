968a28114b2f7b3b0638c872afe1c9f7f8479dcb
Dentaku65 <dentaku65@gmail.com>
2016-06-11 14:58:36 +0200

    re-fixed

--- servers/openload.py
+++ servers/openload.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
-# pelisalacarta - XBMC Plugin
+# streamondemand - XBMC Plugin
 # Conector for openload.co
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 # ------------------------------------------------------------
@@ -12,6 +12,7 @@ import re
 from core import logger
 from core import scrapertools
 
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'}
 
 
@@ -21,7 +22,7 @@ def test_video_exists(page_url):
     data = scrapertools.downloadpageWithoutCookies(page_url)
 
     if 'We are sorry!' in data:
-        return False, "[Openload] File Not Found or Removed"
+        return False, "[Openload] Il file è stato cancellato" 
 
     return True, ""
 
@@ -30,43 +31,36 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.info("streamondemand.servers.openload url=" + page_url)
     video_urls = []
 
-    video = True
     data = scrapertools.downloadpageWithoutCookies(page_url)
+    subtitle = scrapertools.find_single_match(data, '<track kind="captions" src="([^"]+)" srclang="es"')
+    #Header para la descarga
+    header_down = "|User-Agent="+headers['User-Agent']+"|"
 
-    urls_check = []
     if "videocontainer" not in data:
-        video = False
-        url = page_url.replace("/embed/", "/f/")
+        url = page_url.replace("/embed/","/f/")
         data = scrapertools.downloadpageWithoutCookies(url)
-        text_encode = scrapertools.get_match(data, "Click to start Download.*?<script[^>]+>(.*?)</script")
+        text_encode = scrapertools.find_single_match(data,"Click to start Download.*?<script[^>]+>(.*?)</script")
         text_decode = decode(text_encode)
-    else:
-        text_encode = scrapertools.find_multiple_matches(data, '<script type="text/javascript">(ﾟωﾟ.*?)</script>')
-        # Se descodifican todos los script para dar con el correcto
-        for text in text_encode:
-            text_decode = decode(text)
-            videourl = scrapertools.find_single_match(text_decode, "(http.*?true)")
-            if videourl:
-                urls_check.append(videourl)
 
-        videourl = urls_check[1]
-
-    subtitle = scrapertools.find_single_match(data, '<track kind="captions" src="([^"]+)" srclang="it"')
-    # Header para la descarga
-    header_down = "|User-Agent=" + headers['User-Agent'] + "|"
-    if video:
-        videourl = scrapertools.get_header_from_response(videourl, header_to_get="location")
-        videourl = videourl.replace("https://", "http://").replace("?mime=true", "")
-        extension = videourl[-4:]
-        video_urls.append([extension + " [Openload]", videourl + header_down + extension, 0, subtitle])
-    else:
         videourl = scrapertools.find_single_match(text_decode, '(http.*?)\}')
-        videourl = videourl.replace("https://", "http://")
-        extension = '.mp4'
-        video_urls.append([extension + " [Openload]", videourl + header_down + extension])
+        videourl = videourl.replace("https://","http://")
+        extension = videourl[-4:]
+        video_urls.append([ extension + " [Openload]", videourl+header_down+extension])
+    else:
+        text_encode = scrapertools.find_multiple_matches(data,'<script type="text/javascript">(ﾟωﾟ.*?)</script>')
+        # Buscamos la variable que nos indica el script correcto
+        subtract = scrapertools.find_single_match(data, 'welikekodi_ya_rly = ([^;]+)')
+        index = eval(subtract)
+        text_decode = decode(text_encode[index])
+        videourl = scrapertools.find_single_match(text_decode, "(http.*?true)")
+     
+        videourl = scrapertools.get_header_from_response(videourl, header_to_get="location")
+        videourl = videourl.replace("https://","http://").replace("?mime=true","")
+        extension = videourl[-4:]
+        video_urls.append([extension + " [Openload] ", videourl+header_down+extension, 0, subtitle])
 
     for video_url in video_urls:
-        logger.info("streamondemand.servers.openload %s - %s" % (video_url[0], video_url[1]))
+        logger.info("streamondemand.servers.openload %s - %s" % (video_url[0],video_url[1]))
 
     return video_urls
 
@@ -102,7 +96,7 @@ def decode(text):
     txt = ""
     for char in chars:
         char = char \
-            .replace("(oﾟｰﾟo)", "u") \
+            .replace("(oﾟｰﾟo)","u") \
             .replace("c", "0") \
             .replace("(ﾟДﾟ)['0']", "c") \
             .replace("ﾟΘﾟ", "1") \
@@ -113,41 +107,42 @@ def decode(text):
             .replace("ﾟｰﾟ", "4") \
             .replace("(+", "(")
         char = re.sub(r'\((\d)\)', r'\1', char)
-        for x in scrapertools.find_multiple_matches(char, '(\(\d\+\d\))'):
-            char = char.replace(x, str(eval(x)))
-        for x in scrapertools.find_multiple_matches(char, '(\(\d\^\d\^\d\))'):
-            char = char.replace(x, str(eval(x)))
-        for x in scrapertools.find_multiple_matches(char, '(\(\d\+\d\+\d\))'):
-            char = char.replace(x, str(eval(x)))
-        for x in scrapertools.find_multiple_matches(char, '(\(\d\+\d\))'):
-            char = char.replace(x, str(eval(x)))
-        for x in scrapertools.find_multiple_matches(char, '(\(\d\-\d\))'):
-            char = char.replace(x, str(eval(x)))
-        if 'u' not in char: txt += char + "|"
-    txt = txt[:-1].replace('+', '')
-    txt_result = "".join([chr(int(n, 8)) for n in txt.split('|')])
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\+\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\^\d\^\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\+\d\+\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\+\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\-\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        if 'u' not in char: txt+= char + "|"
+    txt = txt[:-1].replace('+','')
+    txt_result = "".join([ chr(int(n, 8)) for n in txt.split('|') ])
     sum_base = ""
     m3 = False
     if ".toString(" in txt_result:
-        if "+(" in txt_result:
+        if "+(" in  txt_result:
             m3 = True
-            sum_base = "+" + scrapertools.find_single_match(txt_result, ".toString...(\d+).")
-            txt_pre_temp = scrapertools.find_multiple_matches(txt_result, "..(\d),(\d+).")
-            txt_temp = [(n, b) for b, n in txt_pre_temp]
+            sum_base = "+"+scrapertools.find_single_match(txt_result,".toString...(\d+).")
+            txt_pre_temp = scrapertools.find_multiple_matches(txt_result,"..(\d),(\d+).")
+            txt_temp = [ (n, b) for b ,n in txt_pre_temp ]
         else:
             txt_temp = scrapertools.find_multiple_matches(txt_result, '(\d+)\.0.\w+.([^\)]+).')
         for numero, base in txt_temp:
-            code = toString(int(numero), eval(base + sum_base))
+            code = toString( int(numero), eval(base+sum_base) )
             if m3:
-                txt_result = re.sub(r'"|\+', '', txt_result.replace("(" + base + "," + numero + ")", code))
+                txt_result = re.sub( r'"|\+', '', txt_result.replace("("+base+","+numero+")", code) )
             else:
-                txt_result = re.sub(r"'|\+", '', txt_result.replace(numero + ".0.toString(" + base + ")", code))
+                txt_result = re.sub( r"'|\+", '', txt_result.replace(numero+".0.toString("+base+")", code) )
     return txt_result
 
 
-def toString(number, base):
-    string = "0123456789abcdefghijklmnopqrstuvwxyz"
-    if number < base:
-        return string[number]
-    else:
-        return toString(number // base, base) + string[number % base]
+def toString(number,base):
+   string = "0123456789abcdefghijklmnopqrstuvwxyz"
+   if number < base:
+      return string[number]
+   else:
+      return toString(number//base,base) + string[number%base]
+
